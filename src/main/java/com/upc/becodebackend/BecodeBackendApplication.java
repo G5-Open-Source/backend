eeec78fd91993d0e98d958b7d7c36508237b40c0
Rodrigo Liberato <129563169+Libeman10@users.noreply.github.com>
2025-06-07 12:19:54 -0500

    initial commit

---
+++ src/main/java/com/upc/becodebackend/BecodeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.upc.becodebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecodeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecodeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/becodebackend/BecodeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.becodebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecodeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
