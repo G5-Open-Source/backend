198d4fae49bb7ffd16429d87d51219368e8b85a8
Kaellagos <klagos2004@gmail.com>
2025-06-18 13:04:52 -0500

    Archivos pertenecientes al Subscription context
    Se añadieron los archivos que conforman el subscription context del proyecto, contiene la base de datos, cada entidad tiene su respectiva tabla y se incluye su archivo Pom correspondiente de Microft Edge.

---
+++ src/main/java/com/upc/becodebackend/user/UserApplication.java
@@ -0,0 +1,11 @@
+﻿package com.upc.becodebackend.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
---
+++ src/main/java/com/upc/becodebackend/user/infrastructure/persistence/SubscriptionJpaRepository.java
@@ -0,0 +1,8 @@
+﻿package com.upc.becodebackend.user.infrastructure.persistence;
+
+import com.upc.becodebackend.user.infrastructure.persistence.models.SubscriptionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscriptionJpaRepository extends JpaRepository<SubscriptionEntity, Long> {
+    SubscriptionEntity findByUserId(String userId);
+}
