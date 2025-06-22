30b16ffb1ab409537fe902842e99a9021d58a151
Rodrigo Liberato <129563169+Libeman10@users.noreply.github.com>
2025-06-21 22:17:51 -0500

    fix(User): Delete Suscription

--- src/main/java/com/upc/becodebackend/user/domain/repositories/SubscriptionRepository.java
+++
@@ -1,8 +0,0 @@
-﻿package com.upc.becodebackend.user.domain.repositories;
-
-import com.upc.becodebackend.user.domain.entities.Subscription;
-
-public interface SubscriptionRepository {
-    Subscription save(Subscription subscription);
-    Subscription findByUserId(String userId);
-}
--- src/main/java/com/upc/becodebackend/user/infrastructure/persistence/SubscriptionJpaRepository.java
+++
@@ -1,8 +0,0 @@
-﻿package com.upc.becodebackend.user.infrastructure.persistence;
-
-import com.upc.becodebackend.user.infrastructure.persistence.models.SubscriptionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionJpaRepository extends JpaRepository<SubscriptionEntity, Long> {
-    SubscriptionEntity findByUserId(String userId);
-}
