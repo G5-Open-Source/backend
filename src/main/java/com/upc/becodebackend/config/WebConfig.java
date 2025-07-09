c3830ad97b16e1f316fe42185421db007c8d1303
Rodrigo Liberato <129563169+Libeman10@users.noreply.github.com>
2025-07-09 17:56:11 -0500

    feat: added cors

---
+++ src/main/java/com/upc/becodebackend/config/WebConfig.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.upc.becodebackend.config;
+ 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author 51924
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica a todas las rutas
+                    .allowedOrigins("*")   // Permite todos los orígenes
+                    .allowedMethods("*")   // Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
+                    .allowedHeaders("*");  // Permite todos los headers
+            }
+        };
+    }
+}
